7b18cf180b287e7ad7d3ce2aa3c456fec9306e4f
Edward Noe <eln4jtt@virginia.edu>
2021-01-23 21:19:55 -0500

    Add spring app

---
+++ src/main/java/com/donationspark/donationspark/DonationSparkApplication.java
@@ -0,0 +1,13 @@
+package com.donationspark.donationspark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationSparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonationSparkApplication.class, args);
+	}
+
+}
