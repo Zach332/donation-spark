8c1c1016aca7358eb8147649983a026c77abb0cf
Edward Noe <eln4jtt@virginia.edu>
2021-01-24 00:01:04 -0500

    Add test IEX API call

--- src/main/java/com/donationspark/donationspark/api/DonationController.java
+++ src/main/java/com/donationspark/donationspark/api/DonationController.java
@@ -1,13 +1,25 @@
 package com.donationspark.donationspark.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class DonationController {
+
+    private final RestTemplate restTemplate;
+    private final String IEX_API_TOKEN;
+
+    public DonationController(@Value("${donationspark.iex_api_token}") String IEX_API_TOKEN) {
+        this.restTemplate = new RestTemplate();
+        this.IEX_API_TOKEN = IEX_API_TOKEN;
+    }
     
     @GetMapping("/api/test")
     public String test() {
-        return "Hello world";
+        String url = "https://cloud.iexapis.com/stable/stock/aapl/quote?token=" + IEX_API_TOKEN;
+
+        return restTemplate.getForObject(url, String.class);
     }
 }
