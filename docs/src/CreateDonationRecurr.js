72fff44936d12aadcb217c6ca252d59497e98ef3
Jonathan Hail <jjh3wqt@virginia.edu>
2021-01-24 02:12:23 -0500

    added basic donation to event form

---
+++ docs/src/CreateDonationRecurr.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+export default function CreateDonationRecurr( {donationTrigger} ) {
+
+    return (
+        <div> {/*TODO: CHANGE VARIABLE SUBSTUFF*/}
+            <h2>Donate Everytime {donationTrigger.user} mentions {donationTrigger.keyword}</h2>
+            <div class="mb-3">
+                <label className="form-label">Donation Amount</label>
+                <input className="form-control" type="number" min="1" step="any" placeholder="Enter Amount Here" aria-label="default input example"></input>
+            </div>
+            <div class="mb-3">
+                <label className="form-label">Maximum Donation per Month</label>
+                <input className="form-control" type="number" min="1" step="any" placeholder="Enter Amount Here" aria-label="default input example"></input>
+            </div>
+            <div class="mb-3">
+                <label className="form-label">Donate To</label>
+                <select className="form-select" aria-label="Default select example">
+                    <option value="1">Malaria Consortium</option>
+                    <option value="2">Helen Keller International</option>
+                    <option value="3">New Incentives</option>
+                    <option value="4">SCI Foundation</option>
+                    <option value="5">GiveDirectly</option>
+                </select>
+            </div>
+            <div class="col-auto">
+                <button type="submit" className="btn btn-primary mb-3">Add Event</button>
+            </div>
+        </div>
+    )
+}
