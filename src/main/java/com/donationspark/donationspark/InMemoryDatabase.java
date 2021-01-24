4f1b053a706a6a712f4a771685806fcba4fa6b8a
Edward Noe <eln4jtt@virginia.edu>
2021-01-24 11:36:43 -0500

    Add default triggers

--- src/main/java/com/donationspark/donationspark/InMemoryDatabase.java
+++ src/main/java/com/donationspark/donationspark/InMemoryDatabase.java
@@ -3,6 +3,8 @@ package com.donationspark.donationspark;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.donationspark.donationspark.pojo.KeywordTrigger;
+import com.donationspark.donationspark.pojo.StockPriceChangeTrigger;
 import com.donationspark.donationspark.pojo.Trigger;
 
 import org.springframework.stereotype.Component;
@@ -14,6 +16,18 @@ public class InMemoryDatabase {
 
     public InMemoryDatabase() {
         this.triggers = new ArrayList<>();
+
+        Trigger stockMarketTrigger = new StockPriceChangeTrigger("Stock Market", "Whenever the broader stock market goes up by 2% in one day.", "https://blog.galvanize.com/wp-content/uploads/2020/01/Can-data-science-predict-the-stock-market-.jpg", "VOO", 2.0);
+        stockMarketTrigger.stars = 3209;
+        triggers.add(stockMarketTrigger);
+
+        Trigger starbucksTrigger = new Trigger("Starbucks", "Whenever you buy something at Starbucks.", "https://cdn.vox-cdn.com/thumbor/KFoJMZYDtwzhn3aOEFL__xRq838=/0x0:6000x4000/1200x800/filters:focal(1514x1652:2474x2612)/cdn.vox-cdn.com/uploads/chorus_image/image/64700799/shutterstock_1075910024.0.jpg");
+        starbucksTrigger.stars = 6548;
+        triggers.add(starbucksTrigger);
+
+        Trigger elonMuskTrigger = new KeywordTrigger("Elon Musk", "Whenever Elon Musk tweets about Mars", "https://upload.wikimedia.org/wikipedia/commons/0/02/OSIRIS_Mars_true_color.jpg", "@elonmusk", "Mars");
+        elonMuskTrigger.stars = 4278;
+        triggers.add(elonMuskTrigger);
     }
 
     public List<Trigger> getTriggers() {
