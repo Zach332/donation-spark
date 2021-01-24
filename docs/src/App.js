3bb249a67d11c028e056d21de505617e16422a7c
Zachary <heidelzachary@gmail.com>
2021-01-23 22:42:00 -0500

    Add Navbar and navigation

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './Home';
+import About from './About';
+import Navbar from './Navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Navbar />
+                <div className="container mx-auto">
+                    <Switch>
+                        <Route path="/" exact component={Home} />
+                        <Route component={About} />
+                    </Switch>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
