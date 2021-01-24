a69883dd39f37896bb62717d867e1bbc5cb366ae
Edward Noe <eln4jtt@virginia.edu>
2021-01-24 11:17:43 -0500

    Fix security config again

--- src/main/java/com/donationspark/donationspark/config/WebSecurityConfig.java
+++ src/main/java/com/donationspark/donationspark/config/WebSecurityConfig.java
@@ -10,6 +10,6 @@ public class WebSecurityConfig
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("*").permitAll();
+        http.authorizeRequests().antMatchers("/").permitAll();
     }
 }
